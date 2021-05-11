7f746cffb6e6707fd4d893a692b3e50a04e117dd
nmisustin <nmisustin@gmail.com>
2021-05-11 11:57:37 -0400

    added password authentication

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('just_tech_news_db', 'username', 'password',{
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW,{
     host: 'localhost',
     dialect: 'mysql',
     port: 3306    
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -41,8 +41,26 @@ router.post('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if(!dbUserData){
+            res.status(400).json({message: 'No user with that email address'});
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if(!validPassword){
+            res.status(400).json({message: 'Incorrect password!'})
+        }
+        res.json({user: dbUserData, message: 'You are now logged in'});
+    })
+})
 router.put('/:id', (req, res) => {
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id 
         }
