4a6735a32959d97f65ad2acbbfd93913c10dc404
nmisustin <nmisustin@gmail.com>
2021-05-19 20:09:52 -0400

    finished work in module

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {User} = require('../../models');
+const {User, Post, Vote} = require('../../models');
 
 router.get('/', (req, res) => {
     User.findAll({
@@ -16,7 +16,27 @@ router.get('/:id', (req, res) => {
         attributes: {exclude: ['password']},
         where:{
             id: req.params.id 
-        }
+        }, 
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ]
     })
         .then(dbUserData => {
             if(!dbUserData){
